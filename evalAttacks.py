5f0359fa4a1041c94dba39bb286d35766fc83de5
cyycyy <cyycyy@none.com>
2023-12-11 21:32:24 +0800

    fix

--- evalAttacks.py
+++ evalAttacks.py
@@ -31,8 +31,8 @@ class EvalDataset:
 def evaluate_attack_success_rate_on_model(clean_images, adv_images, model):
     total_samples = len(clean_images)
     successful_attacks = 0
-    clean_loader = TD.DataLoader(clean_images,batch_size=8,shuffle=False)
-    adv_loader = TD.DataLoader(adv_images,batch_size=8,shuffle=False)
+    clean_loader = TD.DataLoader(clean_images,batch_size=16,shuffle=False)
+    adv_loader = TD.DataLoader(adv_images,batch_size=16,shuffle=False)
     for clean_batch, adv_batch in tqdm(zip(clean_loader, adv_loader)):
         clean_batch = toStd(clean_batch).cuda()
         adv_batch = toStd(adv_batch).cuda()
@@ -50,19 +50,19 @@ def evaluate_attack_success_rate_on_model(clean_images, adv_images, model):
 
 
 def evaluate_attack_success_rate(model_names, adv_dir, csv_dir):
-    print(adv_dir)
+    tqdm.write(adv_dir)
     clean_data_path = 'dataset/images/'  # fix
     acc = []
     clean_images = EvalDataset(clean_data_path)
     adv_images = EvalDataset(adv_dir)
     saver = ResultSaver(model_names, adv_dir, csv_dir)
     for model_name in model_names:
-        print(model_name)
+        tqdm.write(model_name)
         model = get_model(model_name)
         success_rate = evaluate_attack_success_rate_on_model(clean_images, adv_images, model)
         acc.append(f'{success_rate:.1%}')
     saver.save(acc)
-    print(acc)
+    tqdm.write(','.join(acc))
 
 
 class ResultSaver:
@@ -73,23 +73,28 @@ class ResultSaver:
         if not os.path.isfile(csv_dir):
             # Create the CSV file and write the header
             with open(csv_dir, 'w') as file:
-                writer = csv.writer(file)
-                writer.writerow(["name"] + model_names)
+                file.write("name," + ','.join(model_names)+'\n')
 
     def save(self, success_rate):
         # Append the data to the CSV file
         with open(self.csv_dir, 'a') as file:
-            writer = csv.writer(file)
-            writer.writerow([self.adv_dir] + success_rate)
+            file.write(f"{self.adv_dir}," + ','.join(success_rate) + '\n')
 
-
+# # 设置 HTTP 代理
+# os.environ['HTTP_PROXY'] = 'http://127.0.0.1:47890'
+# # 设置 HTTPS 代理
+# os.environ['HTTPS_PROXY'] = 'https://127.0.0.1:47890'
 def main():
-    adv_dirs = [
-        # 'dataset/images',  # non-adv
-        # 'adv/tc_FIA_vgg_m3',
-        'adv/tc_NAA_vgg_m3',
-        'adv/tc_LID_vgg_m3',
-    ]
+    # adv_dirs = ['dataset/images',]  # clean input, non-adv.
+    # adv_root='adv_e16'
+    # adv_dirs = [f'{adv_root}/{d}' for d in os.listdir(adv_root)]  # scan
+    # adv_dirs =['LIDTIUW_vgg_m3', 'LIDIIUW_S20_vgg_m3', 'LIDIIUW_S30_vgg_m3', 'LIDII_FGSM_vgg_m3']
+
+    adv_root='adv_e8'
+    adv_dirs =['NAAC0_vgg_m3', 'NAAC5_vgg_m3', 'NAAC10_vgg_m3', 'NAAC15_vgg_m3', 'NAAC20_vgg_m3', 'NAAC25_vgg_m3', 'NAAC30_vgg_m3']
+    adv_dirs = [pj(adv_root,x) for x in adv_dirs]
+    csv_dir = 'log_e8.csv'
+
     model_names = [
         "vgg16",
         "vgg19",
@@ -104,7 +109,6 @@ def main():
         "deit",
         "swin",
     ]
-    csv_dir = 'log.csv'
     for adv_dir in adv_dirs:
         evaluate_attack_success_rate(model_names, adv_dir, csv_dir)
 
